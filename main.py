77dfeed78f9ac8aa63aafeaca686e9b6f341957d
Patrick-Nydegger <160475121+Patrick-Nydegger@users.noreply.github.com>
2025-06-10 23:11:31 +0200

    main

---
+++ main.py
@@ -0,0 +1,30 @@
+
+def main():
+    items = input("Enter Products (comma-separated): ") # erste Iteration, wenn Zeit noch ausbauen
+    input("Press Enter to start the shopping list optimization...")
+
+    get_price()
+    return
+
+def get_api():
+    return
+
+def get_price(items):
+    get_api()
+    for i in items:
+        pass
+        # Datenbankabfrage
+
+
+
+
+def optimize_shopping_list():
+    return product_store_pair
+
+def export_shopping_list(product_store_pair):
+    return
+
+
+
+if __name__ == "__main__":
+    main()
